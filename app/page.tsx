897a243bb3541b617c36f6c75aae9fbfd3b5a332
CroSSer23 <crosserstealer@gmail.com>
2026-03-19 11:02:24 +0000

    feat: set page to force dynamic rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import locations from "@/data/locations";
 import HeroDesktop from "@/components/HeroDesktop";
 import HeroMobile from "@/components/HeroMobile";
